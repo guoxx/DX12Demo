bb23cf3b43c490ddfce86f72b3ceb9d8901acd01
VIRTUOSGAMES\guoxiaoxin <guoxiaoxin@virtuos.com.cn>
2016-09-30 11:03:58 +0800

    Texture creation.

--- DX12Demo/DX12/DX12Texture.cpp
+++ DX12Demo/DX12/DX12Texture.cpp
@@ -1,12 +1,32 @@
 #include "pch.h"
 #include "DX12Texture.h"
 
+#include "DX12Device.h"
+#include "DX12GraphicManager.h"
 
-DX12Texture::DX12Texture()
+
+DX12Texture::DX12Texture(DX12Device* device, DXGI_FORMAT fmt, uint32_t width, uint32_t height)
+	: m_Format{ fmt }
+	, m_Width{ width }
+	, m_Height{ height }
+	, m_MipLevels{ 1 }
 {
-}
+	uint32_t arraySize = 1;
+	uint32_t sampleCount = 1;
+	uint32_t sampleQuality = 0;
+	D3D12_RESOURCE_FLAGS flags = D3D12_RESOURCE_FLAG_NONE;
+	D3D12_TEXTURE_LAYOUT layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+	D3D12_RESOURCE_STATES initialState = D3D12_RESOURCE_STATE_GENERIC_READ;
 
+	m_Resource = device->CreateCommittedTexture2DInDefaultHeap(m_Format, m_Width, m_Height, arraySize, m_MipLevels, sampleCount, sampleQuality, flags, layout, nullptr, initialState);
+}
 
 DX12Texture::~DX12Texture()
 {
+}
+
+void DX12Texture::CreateView(DX12Device * device)
+{
+	m_SRV = DX12GraphicManager::GetInstance()->RegisterResourceInDescriptorHeap(m_Resource.Get(), D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+	device->CreateShaderResourceView(m_Resource.Get(), nullptr, m_SRV.GetCpuHandle());
 }
--- DX12Demo/DX12/DX12Texture.h
+++ DX12Demo/DX12/DX12Texture.h
@@ -1,11 +1,25 @@
 #pragma once
 
 #include "DX12GpuResource.h"
+#include "DX12DescriptorHandle.h"
+
+class DX12Device;
 
 class DX12Texture : public DX12GpuResource
 {
 public:
-	DX12Texture();
+	DX12Texture(DX12Device* device, DXGI_FORMAT fmt, uint32_t width, uint32_t height);
 	virtual ~DX12Texture();
+
+	DX12DescriptorHandle GetSRV() const { return m_SRV; }
+
+private:
+	void CreateView(DX12Device* device);
+
+	DXGI_FORMAT m_Format;
+    uint32_t m_Width;
+    uint32_t m_Height;
+	uint32_t m_MipLevels;
+	DX12DescriptorHandle m_SRV;
 };
 
