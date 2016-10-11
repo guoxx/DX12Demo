807b8e4c5a29a151097584a622367639d0dd7511
guoxiaoxin <guoxiaoxin@virtuos.com.cn>
2016-10-11 09:43:51 +0800

    Basic property of directional light.

--- DX12Demo/3DEngine/Lights/DirectionalLight.h
+++ DX12Demo/3DEngine/Lights/DirectionalLight.h
@@ -7,5 +7,15 @@ class DirectionalLight : public ILight
 public:
 	DirectionalLight();
 	virtual ~DirectionalLight();
+
+	void SetIrradiance(float r, float g, float b) { m_Irradiance = DirectX::XMFLOAT4{r, g, b, 0}; }
+	DirectX::XMFLOAT4 GetIrradiance() const { return m_Irradiance; }
+
+	void SetDirection(float x, float y, float z) { m_Direction = DirectX::XMFLOAT4{x, y, z, 0}; }
+	DirectX::XMFLOAT4 GetDirection() const { return m_Direction; }
+
+private:
+	DirectX::XMFLOAT4 m_Irradiance;
+	DirectX::XMFLOAT4 m_Direction;
 };
 
