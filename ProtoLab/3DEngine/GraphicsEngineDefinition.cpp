817caf7514cf4f68e76ebad8c725a5695ccfdd1f
guoxiaoxin <guoxx@me.com>
2017-07-10 19:45:16 +0800

    WIP

--- ProtoLab/3DEngine/GraphicsEngineDefinition.cpp
+++ ProtoLab/3DEngine/GraphicsEngineDefinition.cpp
@@ -1,18 +1,71 @@
 #include "pch.h"
 #include "GraphicsEngineDefinition.h"
 
+static const char* StrShutterSpeed[NumShutterSpeedEnum] = {
+    "1s",
+    "1/2s",
+    "1/4s",
+    "1/8s",
+    "1/15s",
+    "1/30s",
+    "1/60s",
+    "1/125s",
+    "1/250s",
+    "1/500s",
+    "1/1000s",
+    "1/2000s",
+    "1/4000s",
+};
+
+static const char* StrFStops[NumFStopsEnum] = {
+    "f/1.8",
+    "f/2.0",
+    "f/2.2",
+    "f/2.5",
+    "f/2.8",
+    "f/3.2",
+    "f/3.5",
+    "f/4.0",
+    "f/4.5",
+    "f/5.0",
+    "f/5.6",
+    "f/6.3",
+    "f/7.1",
+    "f/8.0",
+    "f/9.0",
+    "f/10.0",
+    "f/11.0",
+    "f/13.0",
+    "f/14.0",
+    "f/16.0",
+    "f/18.0",
+    "f/20.0",
+    "f/22.0",
+};
+
+static const char* StrISORatings[NumISORatingsEnum] = {
+    "ISO100",
+    "ISO200",
+    "ISO400",
+    "ISO800",
+};
+
 BoolVar g_TiledShading("Graphics/Tiled Shading", true);
 
 BoolVar g_ToneMapping("Graphics/HDR/Tone Mapping", true);
 NumVar g_ToneMapTargetLuminance("Graphics/HDR/Key", 0.08f, 0.01f, 0.99f, 0.01f);
 
 BoolVar g_RSMEnabled("Graphics/RSM/Enable", false);
-NumVar g_RSMSampleRadius("Graphics/RSM/Sample Radius", 0.005f, 0.0f, 1.0f, 0.001);
+NumVar g_RSMSampleRadius("Graphics/RSM/Sample Radius", 0.005f, 0.0f, 1.0f, 0.001f);
 NumVar g_RSMFactor("Graphics/RSM/RSM Factor", 4.0f, 0.0f, 20.0f, 0.5);
 NumVar g_RSMRadiusEnd("Graphics/RSM/Radius End", 200.0f, 0.0f, 4000.0f, 20);
 
-BoolVar g_EVSMEnabled("Graphics/EVSM/Enable", true);
+BoolVar g_EVSMEnabled("Graphics/EVSM/Enable", false);
 NumVar g_EVSMPositiveExponent("Graphics/EVSM/Positive Exponent", 40.0000f, 0.0000f, 100.0000f, 0.1000f);
 NumVar g_EVSMNegativeExponent("Graphics/EVSM/Negative Exponent", 5.0000f, 0.0000f, 100.0000f, 0.1000f);
 NumVar g_LightBleedingReduction("Graphics/EVSM/Light Bleeding Reduction", 0.0000f, 0.0000f, 1.0000f, 0.0100f);
 NumVar g_VSMBias("Graphics/EVSM/VSM Bias (x100)", 0.0100f, 0.0000f, 100.0000f, 0.0010f);
+
+EnumVar g_ShutterSpeed("CameraControls/Shutter Speed", ShutterSpeed1Over1, NumShutterSpeedEnum, StrShutterSpeed);
+EnumVar g_Aperture("CameraControls/Aperture", FStop1Point8, NumFStopsEnum, StrFStops);
+EnumVar g_ISORating("CameraControls/ISO Rating", ISO100, NumISORatingsEnum, StrISORatings);
--- ProtoLab/3DEngine/GraphicsEngineDefinition.h
+++ ProtoLab/3DEngine/GraphicsEngineDefinition.h
@@ -10,6 +10,64 @@ enum ShadingConfiguration
 	ShadingConfiguration_Max,
 };
 
+enum ShutterSpeedEnum
+{
+    ShutterSpeed1Over1 = 0,
+    ShutterSpeed1Over2,
+    ShutterSpeed1Over4,
+    ShutterSpeed1Over8,
+    ShutterSpeed1Over15,
+    ShutterSpeed1Over30,
+    ShutterSpeed1Over60,
+    ShutterSpeed1Over125,
+    ShutterSpeed1Over250,
+    ShutterSpeed1Over500,
+    ShutterSpeed1Over1000,
+    ShutterSpeed1Over2000,
+    ShutterSpeed1Over4000,
+
+    NumShutterSpeedEnum
+};
+
+enum FStopsEnum
+{
+    FStop1Point8 = 0,
+    FStop2Point0,
+    FStop2Point2,
+    FStop2Point5,
+    FStop2Point8,
+    FStop3Point2,
+    FStop3Point5,
+    FStop4Point0,
+    FStop4Point5,
+    FStop5Point0,
+    FStop5Point6,
+    FStop6Point3,
+    FStop7Point1,
+    FStop8Point0,
+    FStop9Point0,
+    FStop10Point0,
+    FStop11Point0,
+    FStop13Point0,
+    FStop14Point0,
+    FStop16Point0,
+    FStop18Point0,
+    FStop20Point0,
+    FStop22Point0,
+
+    NumFStopsEnum,
+};
+
+enum ISORatingsEnum
+{
+    ISO100 = 0,
+    ISO200,
+    ISO400,
+    ISO800,
+
+    NumISORatingsEnum
+};
+
 extern BoolVar g_TiledShading;
 
 extern BoolVar g_ToneMapping;
@@ -25,3 +83,7 @@ extern NumVar g_EVSMPositiveExponent;
 extern NumVar g_EVSMNegativeExponent;
 extern NumVar g_LightBleedingReduction;
 extern NumVar g_VSMBias;
+
+extern EnumVar g_ShutterSpeed;
+extern EnumVar g_Aperture;
+extern EnumVar g_ISORating;
