cbd767d745a3e5edf447157213843c07e8a10c96
guoxiaoxin <guoxx@me.com>
2017-07-21 13:49:13 +0800

    Add spherical coordinate.

---
+++ ProtoLab/3DEngine/SphericalCoordinates.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include "SphericalCoordinates.h"
+
+SphericalCoordinates SphericalCoordinates::FromSphere(DirectX::XMVECTOR coord)
+{
+    DirectX::XMVECTOR normalizedCoord = DirectX::XMVector3Normalize(coord);
+
+    // [0, PI]
+    float zenith = std::acos(DirectX::XMVectorGetY(normalizedCoord));
+
+    // [-PI/2, PI/2]
+    float elevation = DirectX::XM_PIDIV2 - zenith;
+
+    // [0, 2*PI]
+    float azimuth = std::atan2(DirectX::XMVectorGetZ(normalizedCoord), DirectX::XMVectorGetX(normalizedCoord));
+    if (azimuth < 0)
+    {
+        azimuth += DirectX::XM_2PI;
+    }
+
+    SphericalCoordinates sphericalCoord;
+    sphericalCoord.m_Elevation = elevation;
+    sphericalCoord.m_Azimuth= azimuth;
+    return sphericalCoord;
+}
+
+SphericalCoordinates SphericalCoordinates::FromThetaAndPhi(float theta, float phi)
+{
+    assert(0 <= theta && theta <= DirectX::XM_PI);
+    assert(0 <= phi && phi <= DirectX::XM_2PI);
+
+    SphericalCoordinates sphericalCoord;
+    sphericalCoord.m_Elevation = DirectX::XM_PIDIV2 - theta;
+    sphericalCoord.m_Azimuth = phi;
+    return sphericalCoord;
+}
+
+DirectX::XMVECTOR SphericalCoordinates::ToSphere(SphericalCoordinates coord)
+{
+    float y = std::cos(coord.GetZenith());
+    float x = std::sin(coord.GetZenith()) * std::cos(coord.GetAzimuth());
+    float z = std::sin(coord.GetZenith()) * std::sin(coord.GetAzimuth());
+    return DirectX::XMVectorSet(x, y, z, 1);
+}
---
+++ ProtoLab/3DEngine/SphericalCoordinates.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+class SphericalCoordinates
+{
+public:
+    // Right hand coordinate
+    // (1, 0, 0) -> North
+    // (0, 1, 0) -> Zenith
+    // (0, 0, 1) -> East
+    static SphericalCoordinates FromSphere(DirectX::XMVECTOR coord);
+    static SphericalCoordinates FromThetaAndPhi(float theta, float phi);
+    static DirectX::XMVECTOR ToSphere(SphericalCoordinates coord);
+    
+    float GetZenith() const
+    {
+        return DirectX::XM_PIDIV2 - m_Elevation;
+    }
+
+    float GetElevation() const
+    {
+        return m_Elevation;
+    }
+
+    float GetAzimuth() const
+    {
+        return m_Azimuth;
+    }
+
+private:
+    SphericalCoordinates() = default;
+
+    float m_Elevation;
+    float m_Azimuth;
+};
